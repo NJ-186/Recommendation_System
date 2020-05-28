00549eedcb28f15447f30697d12d1a223d7c5f85
zainhan <zainhanjune@gmail.com>
2020-05-29 00:17:48 +0530

    Added left over files

---
+++ recommend/register/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path,re_path
+from . import views;
+
+
+urlpatterns=[
+
+    path('',view.register),
+    path('check/',views.check),
+    path('confirm/',views.confirm),
+    re_path(r'^verify/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        views.verify, name='activate'),
+]
